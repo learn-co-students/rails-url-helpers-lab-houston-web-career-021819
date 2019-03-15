f15fed06d2810cf061edbc9bb36064682fc82915
moranja <jeffadammoran@gmail.com>
2019-03-15 12:14:45 -0500

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,7 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+  skip_before_filter :verify_authenticity_token
+
   def index
     @students = Student.all
   end
@@ -8,7 +9,29 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    @student = set_student
+  end
+
+  def activate_student
+    student = set_student
+    student.toggle(:active)
+    student.save
+    redirect_to :student
+  end
+
+  # def activate_patch
+  #   student = set_student
+  #   student.update(student_params[:student])
+  #   redirect_to ("/students/#{student.id}")
+  # end
+  # first attempt
+
   private
+
+    def student_params
+      params.permit(student: [:first_name, :last_name, :active])
+    end
 
     def set_student
       @student = Student.find(params[:id])
