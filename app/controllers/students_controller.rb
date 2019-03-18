cebfdbde408ac7e5820c4e1d7f4d2eb6bdefc5d8
josieyao <yao.josephine@yahoo.com>
2019-03-18 10:18:15 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def activate
+  def update
     @student = Student.find(params[:id])
 
     if @student.active == false
